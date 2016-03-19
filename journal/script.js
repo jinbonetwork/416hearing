8b86e03e18c4768f2b40dd46487e1029459c0d6d
hodoug <j.hodoug@gmail.com>
2016-03-20 03:42:12 +0900

    320에서 미디어 배열 완료.

--- journal/script.js
+++ journal/script.js
@@ -4,11 +4,61 @@
 			url: 'data/journal.json', dataType: 'json',
 			success: function(sections){
 				makeHtml(sections);
+			},
+			complete: function(){
 				$jn().trigger($.Event('ready'));
 				//$('button.menu-button').trigger('click');
+				adjustImages();
+				adjustVideo();
 			}
 		});
 	});
+	function adjustVideo(){
+		$jn('.medium iframe.video').each(function(){
+			var width = $(this).attr('width');
+			var height = $(this).attr('height');
+			var wrapWidth = $(this).parent().width();
+			var wrapHeight = $(this).parent().height();
+			var ratio = wrapWidth / width;
+			if(height * ratio < wrapHeight){
+				ratio = wrapHeight / height;
+				var nH = height * ratio;
+				var nW = width * ratio;
+				$(this).attr('width', nW);
+				$(this).attr('height', nH);
+				$(this).css({ 'margin-left': (wrapWidth - nW)/2+'px' });
+			} else {
+				var nW = width * ratio;
+				var nH = height * ratio;
+				$(this).attr('width', nW);
+				$(this).attr('height', nH);
+				$(this).css({ 'margin-top': (wrapHeight - nH)/2+'px' });
+			}
+		});
+	}
+	function adjustImages(){
+		$jn('.medium img').each(function(){
+			$(this).load(function() {
+				var width = $(this).width();
+				var height = $(this).height();
+				var wrapWidth = $(this).parent().width();
+				var wrapHeight = $(this).parent().height();
+				if(wrapHeight > height) {
+					var ratio = wrapHeight / height;
+					var nH = height * ratio;
+					var nW = width * ratio;
+					$(this).css({ width: nW, height: nH});
+					$(this).css({ 'margin-left': (wrapWidth-nW)/2+'px' });
+				} else {
+					var ratio = wrapWidth / width;
+					var nH = height * ratio;
+					var nW = width * ratio;
+					$(this).css({ width: nW, height: nH});
+					$(this).css({ 'margin-top': (wrapHeight-nH)/2+'px' });
+				}
+			});
+		});
+	}
 	function makeHtml(sections){
 		var tplSection = _.template($('#section-template').html());
 		var tplArticle = _.template($('#article-template').html());
