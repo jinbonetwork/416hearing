26d808e124343d7a68fef430e667612151bf0ae0
taiji202 <taiji202@gmail.com>
2016-03-23 22:39:39 +0900

    가족협의회 요구사항 반영

--- index.html.php
+++ index.html.php
@@ -50,7 +50,7 @@ function less($hrefs){
 	</nav>
 	<!-- /navigation-->
 	<section class="introduction">
-		<p class="content">감추는 자가 범인이다.<br>끝까지 밝혀야 합니다!</p>
+		<p class="content"><span>감추는 자가 범인이다.</span><br>끝까지 밝혀야 합니다!</p>
 		<p class="credit">만든이들: <a href="https://www.facebook.com/416family">416가족협의회</a> &amp; <a href="http://lab.jinbo.net">진보넷 독립네트워크팀</a></p>
 	</section>
 	<!-- pages stack -->
