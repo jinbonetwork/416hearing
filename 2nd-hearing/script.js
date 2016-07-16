cfb35b945b88e86c1afc2c25541dd01639cc9f43
hodoug <j.hodoug@gmail.com>
2016-07-17 02:54:45 +0900

    slideshow() 리팩토링.

--- 2nd-hearing/script.js
+++ 2nd-hearing/script.js
@@ -512,122 +512,131 @@ var sHearing2;
 	}}//$.fn.scrollSnap()
 
 	$.fn.slideshow = function(arg){
-		if(arg === undefined) arg = {};
-		if(arg.section === undefined) arg.section = 'li';
-		if(arg.ratio === undefined) arg.ratio = 3/4;
-		if(arg.gutter === undefined) arg.gutter = '5%';
-		if(arg.bgcolor === undefined) arg.bgcolor = '#4d4d4d';
-		if(arg.captbgcolor === undefined) arg.captbgcolor = '#4d4d4d';
-
+		(function(){
+			var defaults = {
+				section: 'li',
+				ratio: 3/4,
+				gutter: '5%',
+				bgcolor: '#4d4d4d',
+				captbgcolor: '#4d4d4d'
+			}
+			arg = $.extend({}, defaults, arg);
+		})();
 		$(this).each(function(){
-			slideshow($(this));
+			new Slideshow($(this), arg);
 		});
-
-		function slideshow($contain){
-			var containRatio = arg.ratio;
-			var index = 0;
-			var $sections = $contain.children(arg.section);
-			var $mainWrap = $('<div class="slideshow-wrap"></div>').appendTo($contain).append($sections);
-			var $leftWrap = $('<div class="left"><div class="prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div></div>').insertBefore($mainWrap);
-			var $rightWrap = $('<div class="right"><div class="next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div></div').insertAfter($mainWrap);
-
-			initialize();
-			fitImages();
-			makeCaption();
-			makeArrowAndClickEvent();
-			bindRefreshEvent();
-
-			function initialize(){
-				$contain.css({ height: $contain.width() * containRatio, overflow: 'hidden', position: 'relative' });
-				$leftWrap.css({ width: arg.gutter, height: '100%', float: 'left' });
-				$rightWrap.css({ width: arg.gutter, height: '100%', float: 'left' });
-				var mainWidth = $contain.width() - $leftWrap.width() - $rightWrap.width() - 1;
-				$mainWrap.css({ position: 'relative', width: mainWidth, height: '100%', float: 'left' });
-				$sections.css({ position: 'absolute', top: 0, left: 0, diasplay: 'block',
-					width: '100%', height: '100%', overflow: 'hidden', 'background-color': arg.bgcolor
-				});
-				$sections.children('a').css({ diasplay: 'block', width: '100%', height: '100%', overflow: 'hidden' });
-			}
-			function fitImages(){
-				var maxImgHeight = 0;
-				var numOfLoad = 0;
-				$sections.find('img').each(function(){
-					$(this).load(function(){
-						var $img = $(this);
-						var ratio = $img.height() / $img.width();
-						if(ratio > containRatio) $img.css('height', '100%');
-						else $img.css('width', '100%');
-						if(maxImgHeight < $img.height()) maxImgHeight = $img.height();
-						numOfLoad++;
-						if(numOfLoad == $sections.length){
-							$contain.height(maxImgHeight);
-							containRatio = maxImgHeight / $contain.width();
-							adjustImageMarginAndPlayIcon();
-						}
-					});
-				});
-			}
-			function adjustImageMarginAndPlayIcon(){
-				$sections.find('img').each(function(){
-					var $img = $(this);
-					$img.css({ 'margin-top': '', 'margin-bottom': '', 'margin-left': '', 'margin-right': '' });
-					if($img.width() == $img.parent().width()){
-						var margin = ($img.parent().height() - $img.height())/2;
-						$img.css({ 'margin-top': margin, 'margin-bottom': margin });
-					} else {
-						var margin = ($img.parent().width() - $img.width())/2;
-						$img.css({ 'margin-left': margin, 'margin-right': margin });
-					}
-				});
-				$sections.find('.play-icon').each(function(){
-					$(this).children('i').css({ 'font-size': $(this).height() });
-				});
-			}
-			function makeCaption(){
-				$sections.find('img').each(function(){ if($(this).attr('alt')){
-					var $caption = $('<div class="caption"><span>'+$(this).attr('alt')+'</span></div>').insertAfter($(this));
-					$caption.css({
-						position: 'absolute', bottom: 0, left: 0, width: '100%', height: 'auto', opacity: 0.7, 'background-color': arg.captbgcolor,
-						padding: '0.5em 1em'
-					});
-				}});
-			}
-			function makeArrowAndClickEvent(){ if($sections.length > 1){
-				var $prev = $leftWrap.children('.prev'), $next = $rightWrap.children('.next');
-				$prev.css({ position: 'absolute', 'z-index': '10', height: '10%', width: 'auto', top: '45%', left: 0, cursor: 'pointer' });
-				$next.css({ position: 'absolute', 'z-index': '10', height: '10%', width: 'auto', top: '45%', right: 0, cursor: 'pointer' });
-				$prev.children().css({ 'font-size': $prev.height() });
-				$next.children().css({ 'font-size': $next.height() });
-
-				var isChanging = false;
-				$sections.not(':eq(0)').css({ left: $contain.width() });
-				$next.click(function(){ if(isChanging === false){
-					isChanging = true;
-					var next = ( index < $sections.length-1 ? index+1 : 0 );
-					$sections.eq(index).finish().animate({ left: -1*$contain.width() }, 1000, 'easeOutQuint');
-					$sections.eq(next).css({ left: $contain.width() }).finish().animate({ left: 0 }, 1000, 'easeOutQuint', function(){ isChanging = false; });
-					index = next;
-				}});
-				$prev.click(function(){if(isChanging === false){
-					isChanging = true;
-					var prev = ( index > 0 ? index-1 : $sections.length-1 );
-					$sections.eq(index).finish().animate({ left: $contain.width() }, 1000, 'easeOutQuint');
-					$sections.eq(prev).css({ left: -1*$contain.width() }).finish().animate({ left: 0 }, 1000, 'easeOutQuint', function(){ isChanging = false; });
-					index = prev;
-				}});
-			}}
-			function bindRefreshEvent(){
-				$contain.on('refresh', refresh);
-				$(window).resize(refresh);
-			}
-			function refresh(){ if($contain.is(':visible')){
-				$contain.css({ height: $contain.width()*containRatio });
-				$mainWrap.css({ width: $contain.width() - $leftWrap.width() - $rightWrap.width() - 1 });
-				adjustImageMarginAndPlayIcon();
-				$sections.not(':eq('+index+')').css({ left: $contain.width() });
-			}}
-		}//slideshow
 	}//$.fn.slideshow
+	function Slideshow($contain, arg){
+		this.arg = arg;
+		this.containRatio = this.arg.ratio;
+		this.index = 0;
+		this.$contain = $contain;
+		this.$sections = $contain.children(this.arg.section);
+		this.$mainWrap, this.$leftWrap, this.$rightWrap;
+
+		this.initMarkup();
+		this.initialize();
+		this.fitImages();
+		this.makeCaption();
+		this.makeArrowAndClickEvent();
+		this.bindRefreshEvent();
+	}
+	Slideshow.prototype.initMarkup = function(){
+		this.$mainWrap = $('<div class="slideshow-wrap"></div>').appendTo(this.$contain).append(this.$sections);
+		this.$leftWrap = $('<div class="left"><div class="prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div></div>').insertBefore(this.$mainWrap);
+		this.$rightWrap = $('<div class="right"><div class="next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div></div').insertAfter(this.$mainWrap);
+	}
+	Slideshow.prototype.initialize = function(){
+		this.$contain.css({ height: this.$contain.width() * this.containRatio, overflow: 'hidden', position: 'relative' });
+		this.$leftWrap.css({ width: this.arg.gutter, height: '100%', float: 'left' });
+		this.$rightWrap.css({ width: this.arg.gutter, height: '100%', float: 'left' });
+		var mainWidth = this.$contain.width() - this.$leftWrap.width() - this.$rightWrap.width() - 1;
+		this.$mainWrap.css({ position: 'relative', width: mainWidth, height: '100%', float: 'left' });
+		this.$sections.css({ position: 'absolute', top: 0, left: 0, diasplay: 'block',
+			width: '100%', height: '100%', overflow: 'hidden', 'background-color': this.arg.bgcolor
+		});
+		this.$sections.children('a').css({ diasplay: 'block', width: '100%', height: '100%', overflow: 'hidden' });
+	}
+	Slideshow.prototype.fitImages = function(){
+		var self = this;
+		var maxImgHeight = 0;
+		var numOfLoad = 0;
+		var $sections = this.$sections;
+		self.$sections.find('img').load(function(){
+			var $img = $(this);
+			var ratio = $img.height() / $img.width();
+			if(ratio > self.containRatio) $img.css('height', '100%');
+			else $img.css('width', '100%');
+			if(maxImgHeight < $img.height()) maxImgHeight = $img.height();
+			numOfLoad++;
+			if(numOfLoad == self.$sections.length){
+				self.$contain.height(maxImgHeight);
+				self.containRatio = maxImgHeight / self.$contain.width();
+				self.adjustImageMarginAndPlayIcon();
+			}
+		});
+	}
+	Slideshow.prototype.adjustImageMarginAndPlayIcon = function(){
+		this.$sections.find('img').each(function(){
+			var $img = $(this);
+			$img.css({ 'margin-top': '', 'margin-bottom': '', 'margin-left': '', 'margin-right': '' });
+			if($img.width() == $img.parent().width()){
+				var margin = ($img.parent().height() - $img.height())/2;
+				$img.css({ 'margin-top': margin, 'margin-bottom': margin });
+			} else {
+				var margin = ($img.parent().width() - $img.width())/2;
+				$img.css({ 'margin-left': margin, 'margin-right': margin });
+			}
+		});
+		this.$sections.find('.play-icon').each(function(){
+			$(this).children('i').css({ 'font-size': $(this).height() });
+		});
+	}
+	Slideshow.prototype.makeCaption = function(){
+		var self = this;
+		self.$sections.find('img').each(function(){ if($(this).attr('alt')){
+			var $caption = $('<div class="caption"><span>'+$(this).attr('alt')+'</span></div>').insertAfter($(this));
+			$caption.css({
+				position: 'absolute', bottom: 0, left: 0, width: '100%', height: 'auto', opacity: 0.7, 'background-color': self.arg.captbgcolor,
+				padding: '0.5em 1em'
+			});
+		}});
+	}
+	Slideshow.prototype.makeArrowAndClickEvent = function(){ if(this.$sections.length > 1){
+		var self = this;
+		var $prev = self.$leftWrap.children('.prev'), $next = self.$rightWrap.children('.next');
+		$prev.css({ position: 'absolute', 'z-index': '10', height: '10%', width: 'auto', top: '45%', left: 0, cursor: 'pointer' });
+		$next.css({ position: 'absolute', 'z-index': '10', height: '10%', width: 'auto', top: '45%', right: 0, cursor: 'pointer' });
+		$prev.children().css({ 'font-size': $prev.height() });
+		$next.children().css({ 'font-size': $next.height() });
+
+		var isChanging = false;
+		self.$sections.not(':eq(0)').css({ left: self.$contain.width() });
+		$next.click(function(){ if(isChanging === false){
+			isChanging = true;
+			var next = ( self.index < self.$sections.length-1 ? self.index+1 : 0 );
+			self.$sections.eq(self.index).finish().animate({ left: -1*self.$contain.width() }, 1000, 'easeOutQuint');
+			self.$sections.eq(next).css({ left: self.$contain.width() }).finish().animate({ left: 0 }, 1000, 'easeOutQuint', function(){ isChanging = false; });
+			self.index = next;
+		}});
+		$prev.click(function(){if(isChanging === false){
+			isChanging = true;
+			var prev = ( self.index > 0 ? self.index-1 : self.$sections.length-1 );
+			self.$sections.eq(self.index).finish().animate({ left: self.$contain.width() }, 1000, 'easeOutQuint');
+			self.$sections.eq(prev).css({ left: -1*self.$contain.width() }).finish().animate({ left: 0 }, 1000, 'easeOutQuint', function(){ isChanging = false; });
+			self.index = prev;
+		}});
+	}}
+	Slideshow.prototype.bindRefreshEvent = function(){
+		this.$contain.on('refresh', this.refresh.bind(this));
+		$(window).resize(this.refresh.bind(this));
+	}
+	Slideshow.prototype.refresh = function(){ if(this.$contain.is(':visible')){
+		this.$contain.css({ height: this.$contain.width()*this.containRatio });
+		this.$mainWrap.css({ width: this.$contain.width() - this.$leftWrap.width() - this.$rightWrap.width() - 1 });
+		this.adjustImageMarginAndPlayIcon();
+		this.$sections.not(':eq('+this.index+')').css({ left: this.$contain.width() });
+	}}
 
 	$.fn.sameSizeWithParent = function(selector){
 		if(selector && $.type(selector) === 'string'){
