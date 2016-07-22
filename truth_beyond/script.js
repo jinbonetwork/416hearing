fa73263dd2257b172f2802c02d8acd4ceec1cfeb
hodoug <j.hodoug@gmail.com>
2016-07-23 01:50:12 +0900

    ection.navy-part-1 마크업, 스타일: 버그 수정

--- truth_beyond/script.js
+++ truth_beyond/script.js
@@ -33,7 +33,7 @@ var sTruthBeyond;
 		self.markupJournal();
 	}
 	SewolTruthBeyond.prototype.adjustment = function(){
-		self.$('.navy-p1-image-wrap:last-child').fitEnd(self.$('.navy-p1-image-wrap:first-child'), 'bottom');
+		self.$('.navy-p1-image-wrap:last-child').fitEnd(self.$('.navy-p1-image-wrap:first-child'), 'bottom', 768);
 	}
 	SewolTruthBeyond.prototype.events = function(){
 		var self = this;
@@ -152,42 +152,49 @@ var sTruthBeyond;
 		$el.append(markup);
 	}
 
-	$.fn.fitEnd = function($target, which){
+	$.fn.fitEnd = function($target, which, active){
 		return this.each(function(){
-			new FitEnd($(this), $target, which);
+			new FitEnd($(this), $target, which, active);
 		});
 	}
-	function FitEnd($el, $target, which){
+	function FitEnd($el, $target, which, active){
 		this.$el = $el;
 		this.$target = $target;
 		this.which = which;
+		this.active = active;
+		this.prop = '';
 
+		this.init();
 		this.fitEnd();
 		this.events();
 	}
+	FitEnd.prototype.init = function(){
+		switch(this.which){
+			case 'top': this.prop = 'padding-bottom'; break;
+			case 'bottom': this.prop = 'padding-top'; break;
+			case 'left': this.prop = 'padding-right'; break;
+			case 'right': this.prop = 'padding-left'; break;
+		}
+	}
 	FitEnd.prototype.fitEnd = function(){
-		var self = this;
-		var oEnd1 = self.$el[0].getBoundingClientRect()[self.which];
-		var oEnd2 = self.$target[0].getBoundingClientRect()[self.which];
-		var end1, end2;
-		var intv = setInterval(function(){
-			end1 = self.$el[0].getBoundingClientRect()[self.which];
-			end2 = self.$target[0].getBoundingClientRect()[self.which];
-			if(oEnd1 === end1 && oEnd2 === end2){
-				clearInterval(intv);
-				var prop = '';
-				switch (self.which){
-					case 'top': prop = 'padding-bottom'; break;
-					case 'bottom': prop = 'padding-top'; break;
-					case 'left': prop = 'padding-right'; break;
-					case 'right': prop = 'padding-left'; break;
+		this.$el.css(this.prop, '');
+		if(this.$el.is(':visible') && this.active <= window.innerWidth){
+			var self = this;
+			var oEnd1 = self.$el[0].getBoundingClientRect()[self.which];
+			var oEnd2 = self.$target[0].getBoundingClientRect()[self.which];
+			var end1, end2;
+			var intv = setInterval(function(){
+				end1 = self.$el[0].getBoundingClientRect()[self.which];
+				end2 = self.$target[0].getBoundingClientRect()[self.which];
+				if(oEnd1 === end1 && oEnd2 === end2){
+					clearInterval(intv);
+					self.$el.css(self.prop, '+='+(end2 - end1));
+				} else {
+					oEnd2 = end2;
+					oEnd1 = end1;
 				}
-				self.$el.css(prop, '+='+(end2 - end1));
-			} else {
-				oEnd2 = end2;
-				oEnd1 = end1;
-			}
-		}, 200);
+			}, 200);
+		}
 	}
 	FitEnd.prototype.events = function(){
 		this.$el.on('refresh', this.fitEnd.bind(this));
