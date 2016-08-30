af938102aac9051189e900c10b9fda43ad834c09
Kyuman Hwang <hwangkm@member.jinbo.net>
2016-08-30 16:32:29 +0900

    내가 청문위원 기능 삭제

--- 3rd_hearing/live/live.js
+++ 3rd_hearing/live/live.js
@@ -10,7 +10,6 @@ var sHearingLive;
 		this.settings = $.extend({}, $.fn.sewolhearinglive.defaults, options);
 
 		this.live = this.Root.find('section.live.se-section');
-		this.opinion = this.Root.find('section.opinion.se-section');
 		this.completeInit = false;
 		this.init();
 	}
@@ -24,8 +23,6 @@ var sHearingLive;
 			});
 
 			this.initLive();
-			this.initOpinion();
-			this.go_opinion(1, false);
 		},
 
 		initLive: function() {
@@ -48,144 +45,6 @@ var sHearingLive;
 					console.log(jqXHR.responseText);
 				}
 			});
-		},
-
-		initOpinion: function() {
-			var self = this;
-			this.opinion.find('form#opinion-form').submit(function(e){
-				var TheForm = this;
-				if(TheForm.memo.value == '') {
-					self.opinion.find('#opinion-memo-alert-box').show().text('질문내용을 입력하세요');
-					TheForm.memo.focus();
-					return false;
-				} else {
-					self.opinion.find('#opinion-memo-alert-box').hide().text('');
-				}
-				var url = TheForm.action;
-				var params = 'mode=add&memo='+TheForm.memo.value;
-
-				jQuery.ajax({
-					url: url,
-					method: 'POST',
-					data: params,
-					dataType: 'json',
-					success: function(json) {
-						var errors = parseInt(json.error);
-						if(errors == 1) {
-							jQuery('#opinion-memo-alert-box').show().text(json.message);
-							TheForm.memo.focus();
-						} else if(errors == 2) {
-							jQuery('#opinion-memo-alert-box').show().text(json.message);
-						} else if(errors == 0) {
-							jQuery('#opinion-memo-alert-box').hide().text('');
-							jQuery('#opinion-items-wrap').replaceWith( self.opinion_make(json) );
-							TheForm.memo.value = '';
-						}
-					},
-					complete: function() {
-						self.opinion_init_click_event();
-					},
-					error: function( jqXHR, textStatus, errorThrown ) {
-						console.log(jqXHR);
-						console.log(textStatus);
-						console.log(errorThrown);
-					}
-				});
-
-				return false;
-			});
-		},
-
-		go_opinion: function(page, scroll) {
-			var self = this;
-
-			var url = this.opinion.find('form#opinion-form').attr('action') + '?page=' + page;
-
-			jQuery.ajax({
-				url: url,
-				method: 'GET',
-				dataType: 'json',
-				success: function(data) {
-					if(parseInt(data.error)) {
-						alert(data.message);
-					} else {
-						jQuery('#opinion-items-wrap').replaceWith( self.opinion_make(data) );
-					}
-				},
-				complete: function() {
-					if(scroll){
-						var wrapOffset = jQuery('#opinion-items-wrap').offset();
-						var scrTop = jQuery('#page-3rd-hearing').scrollTop();
-						jQuery('#page-3rd-hearing').animate({ scrollTop: scrTop + wrapOffset.top }, 400);
-					}
-
-					self.opinion_init_click_event();
-				},
-				error: function( jqXHR, textStatus, errorThrown ) {
-					console.log(errorThrown);
-				}
-			});
-		},
-
-		opinion_init_click_event: function() {
-			var self = this;
-			this.opinion.find('#opinion-nav li').each(function(idx) {
-				var li = jQuery(this);
-				if( li.hasClass('active') ) {
-					li.bind('click',function(e) {
-						self.go_opinion( li.attr('data-id'), true );
-					});
-				} else {
-					li.unbind('click');
-				}
-			});
-			if(this.completeInit !== true) this.completeInit = true;
-		},
-
-		opinion_make: function(json) {
-			if(!json.total_cnt) return '';
-
-			var tplopinionitems = _.template($('#opinion-items-template').html());
-			var tplopinionitem = _.template($('#opinion-item-template').html());
-			var tplopinionnav = _.template($('#opinion-nav-template').html());
-
-			var itemsMarkup  = '';
-			for(var i = 0; i < json.opinions.length; i++ ) {
-				var d = new Date(parseInt(json.opinions[i].regdate) * 1000);
-				d = d.getFullYear() + '.' + (d.getMonth()+1) + '.' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-				itemsMarkup += tplopinionitem({
-					id: json.opinions[i].id,
-					memo: json.opinions[i].memo,
-					regdate: d
-				});
-			}
-
-			var navMarkup = tplopinionnav({
-				classname: (json.nav.p_page ? 'active' : 'deactive'),
-				id: json.nav.p_page,
-				label: '이전'
-			});
-			for(var p = json.nav.s_page; p <= json.nav.e_page; p++) {
-				navMarkup += tplopinionnav({
-					classname: (json.page == p ? 'current' : 'active'),
-					id: p,
-					label: p
-				});
-			}
-			navMarkup += tplopinionnav({
-				classname: (json.nav.n_page ? 'active' : 'deactive'),
-				id: json.nav.n_page,
-				label: '다음'
-			});
-
-			var htmlMarkup = tplopinionitems({
-				page: json.page,
-				total_page: json.total_page,
-				items: itemsMarkup,
-				navi: navMarkup
-			});
-
-			return htmlMarkup;
 		},
 
 		initEdit: function() {
